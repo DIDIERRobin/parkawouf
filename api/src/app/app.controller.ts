aa5e08278ccf359c84624e4382846e7505c03380
Robin <dev@pulsarr.fr>
2023-10-21 11:01:09 +0200

    correcting errors in import

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { join } from 'path';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from "@nestjs/serve-static";
