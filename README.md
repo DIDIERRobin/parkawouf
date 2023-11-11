e90ce375387f2e680fe8a680af97d777ce5b8724
Robin <dev@pulsarr.fr>
2023-11-11 12:29:30 +0100

    adding mongodb with docker to initialize it

--- README.md
+++ README.md
@@ -9,12 +9,21 @@ npm install
 ```
 ### API
  - have an instance of mongodb running on your computer
+ - you can use this following command to mount a mongodb instance in a docker container:
+```shell
+docker-compose up -d
+```
  - create a .env file in the root folder following the .env.example file
- - copy your mongodb config in the .env file
+ - the env.example file unmodified will correspond to the docker-compose file.
+ - copy your mongodb config in the .env file if you want to change it.
+ - run the api:
+```shell
+nx serve api
+```
 ### FRONT
 
-### Finnaly
+### FINALY
 
 ```shell
-npm start
+nx serve
 ```
--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppService } from './app.service';
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { DogModule } from "./modules/dog/dog.module";
 import { MyConfigModule } from "./shared/modules/my-config/my-config.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MyConfigService } from "./shared/modules/my-config/my-config.service";
 
 @Module({
   imports: [
     MyConfigModule,
+    MongooseModule.forRootAsync({
+      useFactory: async (myConfigService: MyConfigService) => ({
+        uri: myConfigService.mongodb.uri
+      }),
+      inject: [MyConfigService],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'front'),
       // Exclure le chemin de l'API pour éviter tout conflit
@@ -19,4 +27,5 @@ import { MyConfigModule } from "./shared/modules/my-config/my-config.module";
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
